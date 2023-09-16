07a80e1958cf11de6dffd8c462e4e438604f5a6f
Sanjeev Chauhan <sanjeev.chauhan@duke.edu>
2023-09-16 17:30:04 -0400

    modal test workds

--- test-yt.py
+++ test-yt.py
@@ -1,10 +1,26 @@
 import modal
-from WeaviateLink import VectorDB
+
+image = modal.Image.debian_slim().pip_install(
+    "youtube_transcript_api", "weaviate-client", "google-api-python-client"
+)
+
 stub = modal.Stub("test-weaviate")
 
 
-@stub.local_entrypoint()
-def main():
-    print('test')
+@stub.function(
+    image=image,
+    mounts=[
+        modal.Mount.from_local_dir("classes"),
+        modal.Mount.from_local_python_packages("WeaviateLink"),
+    ],
+)
+def run():
+    from WeaviateLink import VectorDB
+
+    print("test")
     db = VectorDB("http://44.209.9.231:8080")
     print(db.add_playlist("6E9WU9TGrec"))
+
+
+# @stub.local_entrypoint()
+# def main():
