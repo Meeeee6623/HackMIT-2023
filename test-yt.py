4dfe66d8cfc0a7762a9b74bc80bab3e677aec66c
Sanjeev Chauhan <sanjeev.chauhan@duke.edu>
2023-09-16 17:52:37 -0400

    fuly working except google api

--- test-yt.py
+++ test-yt.py
@@ -10,12 +10,29 @@ stub = modal.Stub("test-weaviate")
 @stub.function(
     image=image,
     mounts=[
+<<<<<<< Updated upstream
         modal.Mount.from_local_dir("classes"),
+=======
+        modal.Mount.from_local_dir(
+            "classes",
+            remote_path="/classes",
+        ),
+>>>>>>> Stashed changes
         modal.Mount.from_local_python_packages("WeaviateLink"),
     ],
 )
 def run():
     from WeaviateLink import VectorDB
+<<<<<<< Updated upstream
+=======
+    import os
+
+    for root, dirs, files in os.walk("/"):
+        for d in dirs:
+            if d.startswith("classes"):
+                print(d)
+                print(os.listdir(os.path.join(root, d)))
+>>>>>>> Stashed changes
 
     print("test")
     db = VectorDB("http://44.209.9.231:8080")
