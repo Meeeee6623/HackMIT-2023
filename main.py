4d52ca8f7a9f15b4bf91a61b626cee483b3ccf1d
Sanjeev Chauhan <sanjeev.chauhan@duke.edu>
2023-09-17 00:56:32 -0400

    not done

--- main.py
+++ main.py
@@ -3,12 +3,16 @@ from modal import Image, Mount, Stub, asgi_app, gpu, method
 from pathlib import Path
 from pydantic import BaseModel
 
+import os
+import re
+
+
 
 image = modal.Image.debian_slim().pip_install(
     "youtube_transcript_api", "weaviate-client", "google-api-python-client"
 )
 
-stub = modal.Stub("test-weaviate")
+stub = modal.Stub("yt-search")
 
 
 @stub.function(
@@ -19,6 +23,10 @@ stub = modal.Stub("test-weaviate")
             remote_path="/classes",
         ),
         modal.Mount.from_local_python_packages("WeaviateLink"),
+        modal.Mount.from_local_python_packages("YouTubeLink"),
+        modal.Mount.from_local_python_packages("OpenAILink"),
+
+
         modal.Mount.from_local_dir(
             "frontend",
             remote_path="/root/frontend",
@@ -32,19 +40,73 @@ def app():
     import fastapi.staticfiles
     from fastapi import FastAPI
 
+    from WeaviateLink import VectorDB
+    from OpenAILink import OpenAI_Connector
+    from YouTubeLink import YoutubeConnector
+
+    db = VectorDB("http://44.209.9.231:8080")
+    openai = OpenAI_Connector(os.environ["OPENAI_API_KEY"])
+    yt = YoutubeConnector(os.environ["YOUTUBE_API_KEY"])
+
+
+
+    conversation = []
+
+
     web_app = FastAPI()
+
+
+    @web_app.post("/ask/{query}")
+    async def ask(query: str):
+        # Initialize the variables
+        yt_query = ""
+        tone = ""
+
+    
+        # Get the response from the chatbot
+        response, conversation = openai.get_yt_search(query, conversation)
+
+        # Check for yt query in [] brackets
+        regex = r"\[(.*?)\]"
+        matches = re.findall(regex, response)
+        if len(matches) > 0:
+            continue_chat = False
+            yt_query = matches[0]
+
+        # Check for tone behavior in ** brackets
+        regex = r"\*\*(.*?)\*\*"
+        matches = re.findall(regex, response)
+        if len(matches) > 0:
+            tone = matches[0]
+
+        return {"reply": response, "yt_query": yt_query, "tone": tone}
+
+
 
     @web_app.get("/train/{playlist}")
     async def train_chatbot(playlist: str):
-        # Call the function to train the chatbot using Modal here
-        # For now, just a mock response
+        # Logic from yt-chain.py goes here
+        # For example:
+        playlistID = db.check_playlist(playlist, certainty=0.8)["playlistID"]
+        if not playlistID:
+            # search yt
+            yt_search = openai.get_yt_search(playlist)
+            playlist_list = yt.search_playlists(yt_search)
+            # ... and so on
         return {"message": f"Training started with {playlist}"}
 
     @web_app.get("/chat/{query}")
     async def chat(query: str):
-        # Send the message to the trained chatbot and get the reply
-        # For now, just a mock response
-        reply = f"Chatbot's response to {query}"
+        # Logic from yt-chain.py goes here
+        # For example:
+        videoID = db.search_videos(playlistID, query)['videoID']
+        topic = db.search_topics(videoID, query)
+            # Select the first playlist from the search results
+            selected_playlist = playlist_list[0]
+            # Get the playlist ID
+            playlistID = selected_playlist['id']
+            # Save the playlist to the database
+            db.save_playlist(playlistID, selected_playlist)
         return {"reply": reply}
 
     @web_app.get("/infer/{prompt}")
@@ -55,3 +117,13 @@ def app():
         "/", fastapi.staticfiles.StaticFiles(directory=str(frontend_path), html=True)
     )
     return web_app
+
+
+def get_info(query, conversation):
+    
+
+def setup():
+    db = VectorDB("http://44.209.9.231:8080")
+    openai = OpenAI_Connector(os.environ["OPENAI_API_KEY"])
+    yt = YoutubeConnector(os.environ["YOUTUBE_API_KEY"])
+
--- yt-chain.py
+++ yt-chain.py
@@ -1,21 +1,50 @@
 import os
+import re
 
 from WeaviateLink import VectorDB
 from OpenAILink import OpenAI_Connector
 from YouTubeLink import YoutubeConnector
 
 db = VectorDB("http://44.209.9.231:8080")
-openai = OpenAI_Connector(os.environ['OPENAI_API_KEY'])
-yt = YoutubeConnector(os.environ['YOUTUBE_API_KEY'])
+openai = OpenAI_Connector(os.environ["OPENAI_API_KEY"])
+yt = YoutubeConnector(os.environ["YOUTUBE_API_KEY"])
 
 # input from user
-user_query = input("Welcome to Athena. What would you like to learn about today?\n")
+user_query = input(
+    "Hey, I'm Athena! I can teach you anything on the internet. What do you want to learn?"
+)
+continue_chat = True
+yt_query = ""
+tone = ""
+conversation = []
+while continue_chat:
+    response, conversation = openai.get_yt_search(
+        user_query, conversation
+    )  # text search string
+
+    # check for yt query in [] brackets
+    regex = r"\[(.*?)\]"
+    matches = re.findall(regex, response)
+    if len(matches) > 0:
+        continue_chat = False
+        yt_query = matches[0]
+    # check for tone behavior in ** brackets
+    regex = r"\*\*(.*?)\*\*"
+    matches = re.findall(regex, response)
+    if len(matches) > 0:
+        tone = matches[0]
+
+
 # check if information already in DB
-playlistID = db.check_playlist(user_query, certainty=0.8)['playlistID'] # might need gpt call to modify text/search?
+playlistID = db.check_playlist(user_query, certainty=0.8)[
+    "playlistID"
+]  # might need gpt call to modify text/search?
 if not playlistID:
     # search yt
-    yt_search = openai.get_yt_search(user_query) # text search string
-    playlist_list = yt.search_playlists(yt_search) # format: list of dicts with keys: id, title, description, thumbnail
+    yt_search = openai.get_yt_search(user_query)  # text search string
+    playlist_list = yt.search_playlists(
+        yt_search
+    )  # format: list of dicts with keys: id, title, description, thumbnail
 
     # get playlist ID - ask user for confirmation
     # WILL be replaced with GUI
@@ -24,28 +53,31 @@ if not playlistID:
         print(f"Title: {playlist['title']}, ID: {playlist['id']}")
     playlist_id = input("Which playlist would you like to learn from? (Enter ID)")
     # get correct playlist object
-    playlist = [playlist for playlist in playlist_list if playlist['id'] == playlist_id][0]
+    playlist = [
+        playlist for playlist in playlist_list if playlist["id"] == playlist_id
+    ][0]
 
     # get playlist description and add to database
-    description = openai.get_playlist_search(playlist['title'], playlist['description'])
-    db.add_playlist(playlist_id, playlist['title'], description)
+    description = openai.get_playlist_search(playlist["title"], playlist["description"])
+    db.add_playlist(playlist_id, playlist["title"], description)
 
     # get video IDs for the playlist
     video_list = yt.get_videos(playlist_id)
 
-
     # chunk each video into topics
     for video in video_list:
-        transcript = yt.get_transcript(video['id']) # transcript has keys: text, start, duration
-        topics, video['description'] = openai.get_video_topics(transcript)
+        transcript = yt.get_transcript(
+            video["id"]
+        )  # transcript has keys: text, start, duration
+        topics, video["description"] = openai.get_video_topics(transcript)
         # add to DB
-        db.add_topics(topics, video['id'])
+        db.add_topics(topics, video["id"])
     # upload videos to weaviate
     db.add_videos(video_list, playlist_id)
     pass
 
 # search within playlist
-videoID = db.search_videos(playlistID, user_query)['videoID']
+videoID = db.search_videos(playlistID, user_query)["videoID"]
 
 # search within video
 topic = db.search_topics(videoID, user_query)
